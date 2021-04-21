c586edf07d09e539c76127b8c835e777231c9c18
Terrence Shi <terrenceshi@gmail.com>
2021-04-21 14:38:28 -0400

    added back demo scripts

---
+++ WreckMeIfYouCan/Assets/Scripts/Finish.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+}
