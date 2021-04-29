fcea3e39529fbea8060f5a8d4759355ec720d5cf
sarahcatseye <sarah.y.tsai@gmail.com>
2021-04-29 13:15:29 -0400

    added win cutscene

---
+++ WreckMeIfYouCan/Assets/Scripts/WinCutsceneStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCutsceneStart : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Cam1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TheSequence());
+    }
+
+    IEnumerator TheSequence()
+    {
+        Cam1.SetActive(true);
+        yield return new WaitForSeconds(4.15f);
+    }
+}
