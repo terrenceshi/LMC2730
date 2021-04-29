1b46ada765c49b2e34fa3137860baf3e5d17803d
Terrence Shi <terrenceshi@gmail.com>
2021-04-29 18:11:22 -0400

    GAME SHOULD BE GOOD

--- WreckMeIfYouCan/Assets/Scripts/WinCutsceneStart.cs
+++ WreckMeIfYouCan/Assets/Scripts/WinCutsceneStart.cs
@@ -5,8 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class WinCutsceneStart : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject Cam1;
+    [SerializeField] private GameObject Cam1;
+    [SerializeField] private GameObject blackPanel;
+    [SerializeField] private Animator anim;
+
+    public GameObject wreck;
+    public Animator wreckAnim;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +20,10 @@ public class WinCutsceneStart : MonoBehaviour
     IEnumerator TheSequence()
     {
         Cam1.SetActive(true);
-        yield return new WaitForSeconds(4.15f);
+        yield return new WaitForSeconds(4.5f);
+        anim.SetBool("Fade", true);
+        wreckAnim.SetBool("soundFade", true);
+        yield return new WaitForSeconds(2.25f);
         SceneManager.LoadScene("winScreen");
     }
 }
